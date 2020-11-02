026979ea5eb0a9f177632f1231e9fd9ae8c26674
Graeme Harrison <gharrison365@gmail.com>
2020-11-02 18:50:19 -0500

    Create Xcode project for Swift app GIFR.

---
+++ GifR/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GifR
+//
+//  Created by Graeme Harrison on 2020-11-02.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    
+}
