a1a4463271539e48fe9cb441c5d3581e19af4b05
Graeme Harrison <gharrison365@gmail.com>
2020-11-02 19:18:53 -0500

    Run pod install to create workspace and accompanying CocoaPod files.

---
+++ Pods/Target Support Files/Pods-GifR-GifRUITests/Pods-GifR-GifRUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GifR_GifRUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GifR_GifRUITests
+@end
---
+++ Pods/Target Support Files/Pods-GifR/Pods-GifR-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GifR : NSObject
+@end
+@implementation PodsDummy_Pods_GifR
+@end
