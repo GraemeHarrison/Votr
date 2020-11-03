901166f0f47480158edcbff8b5767437c3d8ed94
Graeme Harrison <gharrison365@gmail.com>
2020-11-02 19:53:46 -0500

    Create basic intro scene with login and signup buttons.

---
+++ GifR/Scenes/Intro/IntroViewController.swift
@@ -0,0 +1,25 @@
+//
+//  IntroViewController.swift
+//  GifR
+//
+//  Created by Graeme Harrison on 2020-11-02.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet var signUpButton: UIButton!
+    @IBOutlet var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+}
