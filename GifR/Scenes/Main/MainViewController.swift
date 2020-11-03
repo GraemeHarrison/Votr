e958dff21112818cb60d3d479706d85e5619023e
Graeme Harrison <gharrison365@gmail.com>
2020-11-02 20:04:56 -0500

    Main view controller: implement methods for showing a particular scene. Hard code to show Intro scene.

--- GifR/Scenes/Main/MainViewController.swift
+++ GifR/Scenes/Main/MainViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    enum Storyboard: String {
+        case intro = "Intro"
     }
 
+    private(set) var currentViewController: UIViewController?
 
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+        showInitialController(storyboard: .intro)
+    }
+
+    private func showInitialController(storyboard: Storyboard) {
+
+        let storyboard = UIStoryboard(name: storyboard.rawValue, bundle: nil)
+        currentViewController = storyboard.instantiateInitialViewController()!
+        showViewController(currentViewController!)
+    }
+
+    func showStoryboard(_ storyboard: Storyboard, storyParameters: Any? = nil) {
+
+        if let vc = currentViewController {
+            hideViewController(vc)
+        }
+        showInitialController(storyboard: storyboard)
+    }
+
+    private func showViewController(_ viewController: UIViewController) {
+
+        addChild(viewController)
+        view.addSubview(viewController.view)
+        configure(subView: viewController.view, into: view)
+        viewController.didMove(toParent: self)
+    }
+
+    private func hideViewController(_ viewController: UIViewController) {
+
+        viewController.willMove(toParent: nil)
+        viewController.view.removeFromSuperview()
+        viewController.removeFromParent()
+    }
+
+    private func configure(subView: UIView, into mainView: UIView) {
+
+        subView.translatesAutoresizingMaskIntoConstraints = false
+
+        var viewBindingsDict = [String: AnyObject]()
+        viewBindingsDict["subView"] = subView
+        mainView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[subView]|", options: [], metrics: nil, views: viewBindingsDict))
+        mainView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[subView]|", options: [], metrics: nil, views: viewBindingsDict))
+    }
 }
-
